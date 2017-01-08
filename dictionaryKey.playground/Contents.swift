6d2797495d7cfecb6b95f227c9158f738b166491
senseiphone17 <senseiphone17@gmail.com>
2017-01-08 23:00:18 +0900

    dictionaryKeys

---
+++ dictionaryKey.playground/Contents.swift
@@ -0,0 +1,18 @@
+/// dictionaryKey
+/// http://qiita.com/keisei_1092/items/c9353c91a90c372bbfac
+
+import UIKit
+
+var dictionary = [
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3",
+    "key4": "value4",
+    "key5": "value5"
+]
+
+print(dictionary.keys)
+print("")
+
+var keys = [String](dictionary.keys)
+print(keys)
