66c96a24c964aac8aedcbc9f25d949b6005bbb4e
senseiphone17 <senseiphone17@gmail.com>
2017-01-27 22:31:52 +0900

    chapter5_3_4

--- MyPlayground5_3_4.playground/Contents.swift
+++ MyPlayground5_3_4.playground/Contents.swift
@@ -9,6 +9,12 @@ print(arrayB) // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 var arrayC = [Character]("いろはにほへと".characters)
 print(arrayC) // ["い", "ろ", "は", "に", "ほ", "へ", "と"]
 
+print(arrayC.count) // 7
+print(arrayC.first!) // い
+print(arrayC.last!) // と
+print(arrayC.isEmpty) // false
+
+
 var arrayD = [Double](repeating:5.0, count:7)
 print(arrayD) // [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0]
 
