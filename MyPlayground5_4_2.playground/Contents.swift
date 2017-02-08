6a804210f839d688dadf6bcbe17a4d3f6e8adda3
senseiphone17 <senseiphone17@gmail.com>
2017-02-08 23:32:05 +0900

    chapter5_4_3

--- MyPlayground5_4_2.playground/Contents.swift
+++ MyPlayground5_4_2.playground/Contents.swift
@@ -5,3 +5,12 @@ let ku: Character = "\u{30AF}\u{3099}"
 print(ku)
 
 print(gu == ku)
+
+func printStringPerCharacters(_ str: String) {
+    for character in str.characters {
+        print(character)
+    }
+}
+
+printStringPerCharacters("いろはにほへと") // い\nろ\nは\nに\nほ\nへ\nと
+
