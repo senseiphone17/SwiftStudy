aa30b1f2c25d37c74e8974484bb470b71a5da18b
senseiphone17 <senseiphone17@gmail.com>
2017-02-15 21:51:52 +0900

    chapter5_5_1

--- MyPlayground5_5_1.playground/Contents.swift
+++ MyPlayground5_5_1.playground/Contents.swift
@@ -1,2 +1,6 @@
 var dictionary = ["Swift": 2014, "Objective-C": 1983]
 print(dictionary) // ["Objective-C": 1983, "Swift": 2014]
+
+var dicStringAndInt = [String: Int]()
+print(dicStringAndInt) // [:]
+print(dicStringAndInt.count) // 0
