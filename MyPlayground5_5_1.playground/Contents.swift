cfc54a3a427ba8f6e6b3ca2ac680e9591af28312
senseiphone17 <senseiphone17@gmail.com>
2017-02-16 23:01:23 +0900

    chapter5_5_1

--- MyPlayground5_5_1.playground/Contents.swift
+++ MyPlayground5_5_1.playground/Contents.swift
@@ -1,6 +1,16 @@
 var dictionary = ["Swift": 2014, "Objective-C": 1983]
 print(dictionary) // ["Objective-C": 1983, "Swift": 2014]
 
+if let value = dictionary["Swift"] {
+    print("hoge") // hoge
+    print(value) // 2014
+}
+
+if let value = dictionary["Java"] {
+    print("hoge") // どちらも出力されない
+    print(value)  //
+}
+
 var dicStringAndInt = [String: Int]()
 print(dicStringAndInt) // [:]
 print(dicStringAndInt.count) // 0
