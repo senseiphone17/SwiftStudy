b84d7a1078e6c91bffa7c6de7dc5d0c8ffac7eaf
senseiphone17 <senseiphone17@gmail.com>
2017-02-27 22:49:33 +0900

    chapter6_1_1

--- MyPlayground6_1_1.playground/Contents.swift
+++ MyPlayground6_1_1.playground/Contents.swift
@@ -27,3 +27,25 @@ for day in [(5, 4), (5, 3), (8, 1), (8, 31)] { // 祝日ではない、　憲法
     default: print("祝日ではない")
     }
 }
+
+print("")
+
+func dayOfWeek(_ y:Int, _ m:Int, _ d:Int) -> Int {
+
+    var y = y, m = m
+    if m < 3 {
+        m += 12; y -= 1
+    }
+    
+    let leap = y + y / 4 - y / 100 + y / 400
+    return (leap + (13 * m + 8) / 5 + d) % 7
+}
+
+var year = 2017
+for day in [(1, 8), (1, 9), (1, 10), (1, 11)] { // 祝日ではない、　成人の日、　祝日ではない、　祝日ではない
+    
+    switch day {
+    case (1, let d) where d >= 8 && d <= 14 && dayOfWeek(year, 1, d) == 1: print("成人の日")
+    default: print("祝日ではない")
+    }
+}
