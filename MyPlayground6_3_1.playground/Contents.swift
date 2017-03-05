afb0dfcf3828d98849d7b44660354b94e2085a73
senseiphone17 <senseiphone17@gmail.com>
2017-03-05 22:55:54 +0900

    chapter6_3_1

--- MyPlayground6_3_1.playground/Contents.swift
+++ MyPlayground6_3_1.playground/Contents.swift
@@ -3,8 +3,20 @@ enum Direction {
     case east
     case south
     case west
+    
+    func clockwise() -> Direction {
+        switch self {
+        case .north: return .east
+        case .east:  return .south
+        case .south: return .west
+        case .west:  return .north
+        }
+    }
 }
 
 let north = Direction.north
 var east: Direction = .east
 print(north == east) // false
+
+print(north.clockwise()) // ease
+print(east.clockwise())  // south
