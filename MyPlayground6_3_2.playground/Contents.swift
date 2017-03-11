28c44dfe8b97d57454d553da0c7e2b8b80ccfa55
senseiphone17 <senseiphone17@gmail.com>
2017-03-11 16:27:24 +0900

    chapter6_3_2

--- MyPlayground6_3_2.playground/Contents.swift
+++ MyPlayground6_3_2.playground/Contents.swift
@@ -1,22 +1,34 @@
 enum Ticket {
     case ticket(Int, Bool, isCoupon: Bool) // 価格 小人 回数ケンカ
-//    case card(Int, Bool) // 残高 小人
+    case card(Int, Bool) // 残高 小人
     case pass
 }
 
-
-
-
 var t1: Ticket = Ticket.ticket(100, true, isCoupon: false)
 var t2: Ticket = Ticket.pass
+var t3: Ticket = Ticket.card(100, true)
+var t4: Ticket = Ticket.card(220, false)
+var t5: Ticket = Ticket.card(200, true)
 
-for t in [t1, t2] { // 普通券:100 小人  バス
+
+for t in [t1, t2, t3, t4, t5] {
 
     switch t {
     case let Ticket.ticket(fare, flag, _):
         print("普通券:\(fare) " + (flag ? "小人" : "大人"))
     case Ticket.pass:
         print("パス")
+    case Ticket.card(let r, true) where r < 110:
+        print("カード小人: 残高不足")
+    case Ticket.card(let r, false) where r < 230:
+        print("カード大人: 残高不足")
+    case Ticket.card:
+        print("カード")
     }
-
 }
+
+// 普通券:100 小人
+// パス
+// カード小人: 残高不足
+// カード大人: 残高不足
+// カード
