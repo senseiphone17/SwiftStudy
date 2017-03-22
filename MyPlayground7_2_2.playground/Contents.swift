3dc4b32c8c6e602020b22018bb2e81e77308f2bd
senseiphone17 <senseiphone17@gmail.com>
2017-03-22 22:44:03 +0900

    chapter7_2_2

--- MyPlayground7_2_2.playground/Contents.swift
+++ MyPlayground7_2_2.playground/Contents.swift
@@ -16,4 +16,16 @@ if ( true ^^ false ) {
     print("false")
 }
 
-// contribution
+precedencegroup RoundPrecedence {
+    higherThan: AdditionPrecedence
+    lowerThan: MultiplicationPrecedence
+}
+
+infix operator %% : RoundPrecedence
+
+func %% (lhs: Int, rhs: Int) -> Int {
+    return lhs - lhs % rhs
+}
+
+print(283548328 %% 100) // 283548300
+print(6 + 2335 %% 10 * 2) // 2326
