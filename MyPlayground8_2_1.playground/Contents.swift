ec132240c65b9f97d15cb66a0def03d637974d75
senseiphone17 <senseiphone17@gmail.com>
2017-04-03 21:46:38 +0900

    chapter8_2_1

--- MyPlayground8_2_1.playground/Contents.swift
+++ MyPlayground8_2_1.playground/Contents.swift
@@ -21,3 +21,28 @@ class DayOfMonth : CustomStringConvertible {
 
 var domA = DayOfMonth.init(month: 12, day: 1)
 print(domA) // 12/01
+
+func dayOfWeek(_ y: Int, _ m: Int, _ d: Int) -> Int {
+    var y = y, m = m
+    
+    if m < 3 {
+        m += 12; y -= 1
+    }
+    
+    let leap = y + y / 4 - y / 100 + y / 400
+    return (leap + (13 * m + 8) / 5 + d) % 7
+}
+
+class DateInfo : DayOfMonth {
+    var year: Int
+    var dow: Int
+    init(_ y: Int, _ m: Int, _ d: Int) {
+        year = y
+        dow = dayOfWeek(y, m, d)
+        super.init(month: m, day: d)
+    }
+}
+
+var diA = DateInfo.init(2017, 4, 3)
+print(diA) // 04/03
+print(diA.dow) // 1
