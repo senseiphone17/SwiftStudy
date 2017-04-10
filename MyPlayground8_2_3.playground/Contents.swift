9837b11350ef54da46f78e30fc3d99ae46b92275
senseiphone17 <senseiphone17@gmail.com>
2017-04-10 22:38:55 +0900

    chapter8_2_3

--- MyPlayground8_2_3.playground/Contents.swift
+++ MyPlayground8_2_3.playground/Contents.swift
@@ -22,3 +22,27 @@ print(plateA.name) // guest
 var plateB = NamePlate.init(name: "Takeshi", title: "Welcome")
 print(plateB?.name) // Takeshi
 print(plateB?.title) // Welcome
+
+class SpeakerNamePlate: NamePlate {
+    override init?(name: String, title: String) {
+        if title == "" {
+            return nil
+        }
+        
+        super.init(name: name, title: title)
+    }
+    
+    convenience init?(name: String) {
+        if name == "" {
+            return nil
+        }
+        self.init(name: name, title: "Speaker")
+    }
+}
+
+let plateC = NamePlate(name: "Thomas", title: "hero")!
+print(plateC.name) // Thomas
+print(plateC.title) // hero
+
+let plateD = NamePlate(name: "", title: "")
+print(plateD) // nil
