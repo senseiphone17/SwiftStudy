8487d0da13b1198ca1cf176223f08da6171b1bd4
senseiphone17 <senseiphone17@gmail.com>
2017-05-07 21:05:44 +0900

    chapter9_2_1 170507

---
+++ MyPlayground9_2_1.playground/Contents.swift
@@ -0,0 +1,18 @@
+struct DoNothing {
+    var array: [Int]
+    init(_ a: [Int]) { array = a }
+    
+    mutating func trouble(_ x: Int) -> Int {
+        // array[x] = 0
+        return array[x - 1]
+    }
+}
+
+var la = [Int](0 ..< 0x20_0000)
+var mom = 0
+for x in 1...500 {
+    var na = DoNothing(la)
+    mom += na.trouble(x)
+}
+
+print(mom)
