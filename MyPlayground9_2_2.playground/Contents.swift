80d3ec78af1efd5db8f5d5f83eeae7369b0c0345
senseiphone17 <senseiphone17@gmail.com>
2017-05-09 22:51:51 +0900

    chapter9_2_2 170509

---
+++ MyPlayground9_2_2.playground/Contents.swift
@@ -0,0 +1,22 @@
+class Student: CustomStringConvertible {
+    let name:String
+    var club: Club?
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func description-> String {
+        var s = "\(name)"
+        
+        if let mem = club {
+            s += ", \(mem.name)"
+        }
+        
+        return s
+    }
+
+    deinit {
+        print("\(name): deinit")
+    }
+}
