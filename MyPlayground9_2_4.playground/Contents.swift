0dae9bf9be3d7ccfb38e3212eb0bc4b47584b38a
senseiphone17 <senseiphone17@gmail.com>
2017-05-21 23:10:50 +0900

    chapter9_2_4 170521

---
+++ MyPlayground9_2_4.playground/Contents.swift
@@ -0,0 +1,49 @@
+class Student: CustomStringConvertible {
+    let name:String
+    weak var club: Club?
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    var description: String {
+                var s = "\(name)"
+        
+                if let mem = club {
+                    s += ", \(mem.name)"
+                }
+                
+                return s
+    }
+
+    deinit {
+        print("\(name): deinit")
+    }
+}
+
+class Club {
+    let name: String
+    var members = [Student]()
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func add(_ p: Student) {
+        members.append(p)
+        p.club = self
+    }
+    
+    deinit {
+        print("Club \(name): deinit")
+    }
+}
+
+weak var who: Student?
+
+do {
+    let kaz: Student = Student(name: "一美")
+    who = kaz
+    print(who!)
+}
+print(who)
