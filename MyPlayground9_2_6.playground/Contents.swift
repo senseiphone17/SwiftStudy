4ca1196e588723bcd082b727eaef02b1e2349ba7
senseiphone17 <senseiphone17@gmail.com>
2017-05-24 21:30:53 +0900

    chapter9_2_6 170524

---
+++ MyPlayground9_2_6.playground/Contents.swift
@@ -0,0 +1,33 @@
+class Student: CustomStringConvertible {
+    let name: String
+    unowned var homeroom: HomeRoom
+    
+    init(name: String, homeroom: HomeRoom) {
+        self.name = name
+        self.homeroom = homeroom
+    }
+    
+    var description: String {
+        return "\(name), \(homeroom.name)"
+    }
+    
+    deinit {
+        print("\(name): deinit")
+    }
+}
+
+class HomeRoom {
+    let name: String
+    var members = [Student]()
+    init(name: String) {
+        self.name = name
+    }
+    
+    func add(_ p: Student) {
+        members.append(p)
+    }
+    
+    deinit {
+        print("HomeRoom \(name): deinit")
+    }
+}
