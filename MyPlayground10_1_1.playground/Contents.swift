aac52998845573ec07eef91a7a7d31012c033130
senseiphone17 <senseiphone17@gmail.com>
2017-09-03 14:11:34 +0900

    chapter10_1_1 add class

--- MyPlayground10_1_1.playground/Contents.swift
+++ MyPlayground10_1_1.playground/Contents.swift
@@ -8,3 +8,18 @@ protocol Human {
     init(name: String)
     func sayHello(to: Human)
 }
+
+class Citizen : Human {
+    let name: String
+    var petPhrase: String = "なるほど"
+    required init(name: String) {
+        self.name = name
+    }
+    
+    func sayHello(to p:Human) {
+        print("どうも、" + p.name + "さん。")
+    }
+}
+
+let simin = Citizen(name: "太郎")
+simin.sayHello(to: simin) // どうも、太郎さん。
